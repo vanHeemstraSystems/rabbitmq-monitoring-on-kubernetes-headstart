596c3d63e63698dc21e8e82b67d7963f892a633b
Willem van Heemstra <willem@vanheemstrasystems.com>
2021-02-08 15:45:14 +0100

    Update SpringAMQPPublisherApplication.java

--- consumer/src/main/java/com/vanheemstrasystems/amqp/SpringAMQPPublisherApplication.java
+++ consumer/src/main/java/com/vanheemstrasystems/amqp/SpringAMQPPublisherApplication.java
@@ -3,6 +3,6 @@ package com.vanheemstrasystems.amqp;
 public class SpringAMQPPublisherApplication {
 
     public static void main(String[], args) {
-        System.out.println("Hello from SpringAMQPPublisherApplication!");
+        System.out.println("Hello from SpringAMQPPublisher!");
     }
 }
