0c7e824b4c98cc2ba11feda9d666fa6123efe2b2
Willem van Heemstra <willem@vanheemstrasystems.com>
2021-02-08 15:26:21 +0100

    Create PublisherApplication.java

---
+++ consumer/src/main/java/com/vanheemstrasystems/amqp/publisher/PublisherApplication.java
@@ -0,0 +1,9 @@
+package com.vanheemstrasystems.amqp.publisher;
+
+public class PublisherApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Publisher!");
+    }
+
+}
